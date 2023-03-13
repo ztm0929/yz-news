284cae2204935dfc5546fc34fcc96bf102ac9221
ztm0929 <ztm0929@icloud.com>
2023-03-14 03:41:09 +0800

    second commit 新增部分注释

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+#  创建 CSV 文件并填写表头，用于存放或获取到的信息，其中 encoding 需要设置为 ‘utf-8-sig’ ，否则Excel读取到中文会显示为乱码，下同；
 with open(f'yz-news/info.csv', 'w', newline='', encoding='utf-8-sig') as f:
     writer = csv.DictWriter(f, fieldnames=['文章标题', '发布时间', '原文链接'])
     writer.writeheader()
 
+# 研招网官网，以及「考研动态」栏目的网址，后者50条新闻为一页，以此递增实现翻页；
 baseurl = 'https://yz.chsi.com.cn'
 url = 'https://yz.chsi.com.cn/kyzx/kydt/?start=0'
 
 content = requests.get(url).text
 soup = BeautifulSoup(content, 'lxml')
 
-# items = soup.find_all('li')
 items = soup.find('ul', class_='news-list').find_all('li')
 
-for index, item in enumerate(items):
-    infolist = []
+for item in items:
+    # 原网页 html 中，标题和链接有分属于不同的 a 标签这一情况，故采用倒序索引的方式获取标题；
     title = item.find_all('a')[-1].text
     # if "初试" in title:
     published_time = item.find('span').text
@@ -27,9 +28,7 @@ for index, item in enumerate(items):
         '发布时间': published_time,
         '原文链接': link,
     }
-    # infolist = infolist.append(info)
-    # # df = pd.DataFrame(info)
-    # # df.to_csv('info.csv')
+    # 打开先前已创建好的CSV文件，并开启追加模式，添加内容即可；
     with open(f'yz-news/info.csv', 'a', newline='', encoding='utf-8-sig') as f:
         writer = csv.DictWriter(f, fieldnames=info.keys())
         writer.writerow(info)
